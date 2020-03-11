849f01dc34e420f97e2759c5172e381cbe033d57
Nikita Ivanov <krasota156@gmail.com>
2020-03-11 18:22:28 +0300

    Initial Commit

---
+++ cxxreflect/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cxxreflect
+//
+//  Created by Nikita Ivanov on 3/11/20.
+//  Copyright © 2020 Plak Corp. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
