cda1af6c63da19dd0ea252b52d31a1e0625e786d
Nikita Ivanov <krasota156@gmail.com>
2020-03-14 17:56:44 +0300

    ifaces soon

---
+++ cxxreflect/CXFramework/Interface.h
@@ -0,0 +1,14 @@
+//
+//  Interface.h
+//  cxxreflect
+//
+//  Created by Nikita Ivanov on 3/14/20.
+//  Copyright © 2020 Plak Corp. All rights reserved.
+//
+
+#ifndef Interface_h
+#define Interface_h
+
+// todo lol
+
+#endif /* Interface_h */
